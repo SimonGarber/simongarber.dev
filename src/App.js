46ffaa6444069549d1fd3aa692eb001ae45b7035
Simon Garber <simon.garber@gmail.com>
2019-11-05 13:19:35 -0500

    additional testing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from "react";
 import "./scss/main.scss";
 import "./scss/App.scss";
-import Counter from "./Components/Counter/Counter.js";
-import NavBar from "./Components/NavBar/NavBar.js";
+
 const App = () => {
-  return (
-    <div>
-      <NavBar />
-      <h1>Hello</h1>
-      <Counter />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
