71a40986eeabe34e0567812af6f109cc76f10a7a
Simon Garber <simon.garber@gmail.com>
2019-11-05 13:01:17 -0500

    Updates

---
+++ precache-manifest.0696b25a2d7ec1f74e41d41c2bfc8c1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6ce0aa6912c7b653bd805af26c01d99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41335f96a3a0c674a53a",
+    "url": "/static/css/main.d205054b.chunk.css"
+  },
+  {
+    "revision": "f1b6ba203e282c7a7b41",
+    "url": "/static/js/2.5bddf9ea.chunk.js"
+  },
+  {
+    "revision": "41335f96a3a0c674a53a",
+    "url": "/static/js/main.ba7f82a0.chunk.js"
+  },
+  {
+    "revision": "42960383cc5959c8e4e3",
+    "url": "/static/js/runtime-main.1f3af2b2.js"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/static/media/background.00051dbb.jpg"
+  }
+]);
