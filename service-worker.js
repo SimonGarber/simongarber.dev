54ded585365c04170f522596105babc13010b49d
Simon Garber <simon.garber@gmail.com>
2019-11-04 19:08:02 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f35c22f0c8d7efce53938b2b377375f1.js"
+  "/precache-manifest.cf3a3744b85f2bd193615e295a2ffce1.js"
 );
 
 self.addEventListener('message', (event) => {
